0a8013e93365e70ddcb3631296de94f605792ba5
Pirasis <1pete@users.noreply.github.com>
2018-08-14 23:23:37 +0700

    add unit testing for `eslint-config-1pete-react`

---
+++ packages/eslint-config-1pete-react/tests/subjects/pass/index.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-unresolved
+import React, { Component } from 'react'
+
+class Hello extends Component {
+  render1() {
+    const hello = 'Hello'
+    const world = 'World'
+
+    return (
+      <div>{hello} {world}</div>
+    )
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default Hello
---
+++ packages/eslint-config-1pete-react/tests/test.js
@@ -0,0 +1,22 @@
+const _ = require('lodash')
+const { CLIEngine } = require('eslint')
+
+const getAllReportedRules = report =>
+  _.chain(report.results)
+    .map('messages')
+    .flatten()
+    .map('ruleId')
+    .uniq()
+    .sort()
+    .value()
+
+describe('eslint config 1pete react', () => {
+  it('test pass', () => {
+    const report = new CLIEngine().executeOnFiles(['./tests/subjects/pass/*.js'])
+
+    if (report.errorCount > 0) {
+      const errors = getAllReportedRules(report)
+      throw new Error(`get errors from:\n\t${errors.join('\n\t')}`)
+    }
+  })
+})
