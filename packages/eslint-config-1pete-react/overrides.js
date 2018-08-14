4a46e4c4b9adf85969e86fe0414353e1edc4cfc5
Pirasis <1pete@users.noreply.github.com>
2018-08-14 23:27:50 +0700

    [override] disable rule `react/forbid-prop-types`

--- packages/eslint-config-1pete-react/overrides.js
+++ packages/eslint-config-1pete-react/overrides.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: '1pete',
   rules: {
     'class-methods-use-this': 'off',
+    'react/forbid-prop-types': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
     'react/jsx-one-expression-per-line': 'off',
   },
